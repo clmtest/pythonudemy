3cac8ac61d32f4292f6716354cf04847d39cf201
Christian <cristiantbon@gmail.com>
2020-08-11 22:26:18 -0500

    my first commit

---
+++ seccion_1.py
@@ -0,0 +1,91 @@
+#print(12.5)
+#print(3*5)
+#print(50/5)
+
+#variable = 5
+#print(variable)
+#print(variable+
+#print(variable*variable)
+
+#print("christian londono munoz")
+#variable = "cristian londono esta aprendiendo phyton"
+#print(type(variable))
+#print ("cristian crea: \n una segunda linea") #al pegar una a \n se elimina el espacio
+#print("cristian tabula \t todo lo que vienes despues de tabular")
+#print("c:\nombres\clases")
+#print(r"c:\nombres\clases") # aplica mucho para cuando se quien dar rutas
+
+#print("""de esta manera nos ahorramos podemos copiar texto y
+#saltar de linea sin la necesidad de utulizar a nun
+#pero ojo que esto no aplica para tabular""")
+
+#variable = "podemos definir la variable con su salto de line \n y este se vera reflejado al imprimir la variable"
+#print(variable)
+#print(variable + variable)
+
+#primero = "primer parte"
+#segundo = " segunda parte"
+#print("fusion de " + primero + segundo)
+
+#alumno = "christian londono munoz"
+#print(alumno[2])
+#print(alumno[8])
+#print(alumno[6])
+#print(alumno[-1]) #tambien funciona a la inversa
+#print(alumno[-3])
+#print(alumno[3:])
+#print(alumno[:5])
+#print(alumno[:]) #trae todo
+#print((alumno[2:])+alumno[:2]) #ojo uno no puede concatenar letras y numero
+#print(len(alumno))
+
+#listas
+
+"""lista = [1,2,3,4,"cristian",6,7,8,9,0,"clm"]
+print(lista[0])
+print(lista[7])
+print(lista[3:])
+print(lista[:8])"""
+
+"""primerparte = [1,2,3]
+segundaparte = [4,5,6,7,8,9]
+print(primerparte+segundaparte)"""
+
+"""numeros = [1,2,3,4,9,6,7,8,9]
+numeros[4] = 5
+numeros.append(10)
+numeros.append(10+1)
+print(numeros)"""
+
+"""abecedario = ["a","b","c","d","e","f","g"]
+print(abecedario)
+print(abecedario[:2])
+abecedario [:2] = ["A", "B"]
+print(abecedario)"""
+
+"""primero = [1,2,3]
+segundo = [4,5,6]
+tercero = [7,8,9]
+cuarto = [10,11,12]
+anidadas = [primero,segundo,tercero,cuarto]
+print(anidadas)
+print(anidadas[0])
+print(anidadas[0][0]) or print(anidadas[0][2])"""
+
+#ingreso de datos por teclado
+
+"""input("CHRISTIAN")
+dato = input("christian")
+print(dato)"""
+
+"""dato = "5"
+dato = int(dato)
+print(dato+5)"""
+
+"""dato = "5.5"
+dato = float(dato)
+print(dato+6.5)"""
+
+"""dato = float(input("introduce un valor decimal o entero: "))
+print(dato)"""
+
